d62d9b41505a3813a186dd76316a74d8f0bb4834
Ksenia Razheva <103069634+KseniaRazheva@users.noreply.github.com>
2023-10-31 12:55:53 +0000

    form added

--- src/components/EventForm/EventForm.tsx
+++ src/components/EventForm/EventForm.tsx
@@ -1,7 +1,24 @@
 import classes from "./EventForm.module.css";
 
 const EventForm = () => {
-    return <div className={classes.component}>EventForm</div>;
+    return <div className={classes.component}>
+        <form>
+            <h2>Registration form</h2>
+            <label>
+                <span>Name</span>
+                <input name="name" type="text" />
+            </label>
+            <label>
+                <span>Email</span>
+                <input name="email" type="email" />
+            </label>
+            <label>
+                <span>Password</span>
+                <input name="password" type="password" />
+            </label>
+            <button>send</button>
+        </form>
+    </div>;
 };
 
 export { EventForm };
