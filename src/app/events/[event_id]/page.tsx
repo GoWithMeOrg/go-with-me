dc89875861ee49ad38800c74e2907992cdd39455
Alex Baumgertner <alex.baumgertner@gmail.com>
2023-10-31 10:26:12 +0000

    chore(*): Add code style check tool
    Added prettier and prettier config file.
    Added npm script to run prettier.
    Added lint staged (runs on commit - Run linters against staged git files and don't let 💩 slip into your code base!
    Fixed code style errors

--- src/app/events/[event_id]/page.tsx
+++ src/app/events/[event_id]/page.tsx
@@ -1,20 +1,24 @@
-'use client'
+"use client";
 
-import { useState } from 'react'
+import { useState } from "react";
 
-import { EventForm } from '@/components/EventForm'
-import { Event } from '@/components/Event'
+import { EventForm } from "@/components/EventForm";
+import { Event } from "@/components/Event";
 
 export default function EventPage() {
-  const [isEditMode, setIsEditMode] = useState(false)
+    const [isEditMode, setIsEditMode] = useState(false);
 
-  return (
-    <div>
-      <h3>EventPage</h3>
-      {isEditMode ? <EventForm /> : <Event /> }
-      <button onClick={() => { setIsEditMode(!isEditMode) }}>
-        Edit
-      </button>
-    </div>
-  )
+    return (
+        <div>
+            <h3>EventPage</h3>
+            {isEditMode ? <EventForm /> : <Event />}
+            <button
+                onClick={() => {
+                    setIsEditMode(!isEditMode);
+                }}
+            >
+                Edit
+            </button>
+        </div>
+    );
 }
