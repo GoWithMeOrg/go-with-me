f38c041d4b4d96d30520ab8e721cd43c120dd2db
Ksenia Razheva <103069634+KseniaRazheva@users.noreply.github.com>
2023-11-01 19:02:25 +0000

    update EventForm

--- src/components/EventForm/EventForm.tsx
+++ src/components/EventForm/EventForm.tsx
@@ -4,20 +4,41 @@ const EventForm = () => {
     return <div className={classes.component}>
         <form>
             <h2>Registration form</h2>
+
             <label>
-                <span>Name</span>
-                <input name="name" type="text" />
+                <span>Trip Name:</span>
+                <input type="text" name="tripName" required />
             </label>
+
             <label>
-                <span>Email</span>
-                <input name="email" type="email" />
+                <span>Description:</span>
+                <textarea name="description">enter text</textarea>
             </label>
+
             <label>
-                <span>Password</span>
-                <input name="password" type="password" />
+                <span>Start date:</span>
+                <input type="date" name="startDate" min="2023-01-01" max="2023-12-31" />
             </label>
-            <button>send</button>
+
+            <label>
+                <span>End date:</span>
+                <input type="date" name="endDate" min="2023-01-01" max="2023-12-31" />
+            </label>
+
+            <label>
+                <span>Is private:</span>
+                <input type="checkbox" name="isPrivate" />
+            </label>
+
+            <label>
+                <span>Banner Image:</span>
+                <input type="file" name="bannerImage" accept=".jpg, .jpeg, .png" />
+            </label>
+
+            <button>Send</button>
+
         </form>
+
     </div>;
 };
 
