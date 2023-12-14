6dbe72b7abe658c107963bb9b843d05466e31f91
banderlog2013 <banderlog2013@yandex.ru>
2023-12-14 23:20:12 +0300

    changed the TrilpList RUST to Graphql

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,5 +1,5 @@
 import { DateTime } from "luxon";
 
-export const formatDate = (date: Date | undefined, format: string): string => {
+export const formatDate = (date: Date | string | undefined, format: string): string => {
     return date ? DateTime.fromISO(date.toString()).toFormat(format) : "";
 };
