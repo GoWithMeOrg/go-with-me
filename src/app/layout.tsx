3cc9bea6911802ca40fff235dd3cac743e7dbbd8
Sergey Platonov <platonov.ser@gmail.com>
2024-03-18 11:12:20 +0100

    Revert "Google maps"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
 import { Menu } from "@/components/Menu";
 import { NextAuthProvider, ApolloWrapper } from "./providers";
+
 import "@/styles/global.css";
 
 const inter = Inter({ subsets: ["latin"] });
