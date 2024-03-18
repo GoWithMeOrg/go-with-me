4bef4e09866c7c0f5474b8ffa62c43ea6f165388
banderlog2013 <banderlog2013@yandex.ru>
2024-03-18 10:53:48 +0300

    bug fix import layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-
 import { Menu } from "@/components/Menu";
 import { NextAuthProvider, ApolloWrapper } from "./providers";
-
 import "@/styles/global.css";
-import { Maps } from "@/components/GoogleMaps/Map";
 
 const inter = Inter({ subsets: ["latin"] });
 
