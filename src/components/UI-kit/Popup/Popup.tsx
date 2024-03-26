fa8113a55fc0cb2bea2fde78e957986603a8624b
AleksandrRazhev <alexandrrazh@gmail.com>
2024-03-26 13:09:44 +0300

    component popup added

---
+++ src/components/UI-kit/Popup/Popup.tsx
@@ -0,0 +1,42 @@
+import { createPortal } from "react-dom";
+import { Dispatch, FC, SetStateAction, useEffect, useState } from "react";
+
+import styles from "./Popup.module.css";
+
+interface PopupProps extends React.HTMLAttributes<HTMLDivElement> {
+    showPopup: boolean;
+    setShowPopup: Dispatch<SetStateAction<boolean>>;
+}
+
+const Popup: FC<PopupProps> = ({ showPopup, setShowPopup, children, ...rest }) => {
+    const [containerState, setContainerState] = useState<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        const body = document.querySelector("body");
+        if (!body) return;
+        const containerExist = document.getElementById("popupContainer");
+        if (showPopup) {
+            if (containerExist) return;
+            const container = document.createElement("div");
+            container.className = styles.popupContainer;
+            container.id = "popupContainer";
+            body.append(container);
+            setContainerState(container);
+        }
+        if (!showPopup) {
+            if (containerExist) containerExist.remove();
+        }
+    }, [showPopup]);
+
+    const render = (
+        <div className={styles.popup} {...{ ...rest }}>
+            {children}
+        </div>
+    );
+    if (!showPopup) return null;
+    if (!containerState) return null;
+
+    return <>{createPortal(render, containerState)}</>;
+};
+
+export default Popup;
