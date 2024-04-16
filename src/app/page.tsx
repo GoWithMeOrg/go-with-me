1ac6ebc64fd76dcf366c660405c4b62a42a75a5b
banderlog2013 <banderlog2013@yandex.ru>
2024-04-17 02:44:19 +0300

    added styles to footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
 "use client";
-import { AuthPanel } from "@/components/AuthPanel";
 
 export default function HomePage() {
-    return (
-        <div className="HomePage">
-            {/* <AuthPanel /> */}
-            {/* Social meetings <a href="https://github.com/GoWithMeOrg/go-with-me">GoWithMeOrg/go-with-me</a> */}
-        </div>
-    );
+    return <main className="HomePage"></main>;
 }
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,50 @@
 import React from "react";
 import classes from "./Footer.module.css";
+import Link from "next/link";
 
 export const Footer = () => {
-    return <footer className={classes.footer}>Footer</footer>;
+    return (
+        <footer className={classes.footer}>
+            <div className={classes.footerContainer}>
+                <div className={classes.footerLogIn}>
+                    <Link href="/profile" className={classes.footerLink}>
+                        Sign In
+                    </Link>
+                    <label className={classes.footerLogo}>Go With Me</label>
+                </div>
+
+                <div className={classes.footerBlockLinks}>
+                    <div className={classes.footerBlock}>
+                        <Link href="/events" className={classes.footerLink}>
+                            Events
+                        </Link>
+
+                        <Link href="/profile" className={classes.footerLink}>
+                            Profile
+                        </Link>
+                    </div>
+                    <div className={classes.footerBlock}>
+                        <Link href="/privacy_policy" className={classes.footerLink}>
+                            Privacy policy
+                        </Link>
+
+                        <Link href="/terms_of_use" className={classes.footerLink}>
+                            Terms of use
+                        </Link>
+                    </div>
+                    <div className={classes.footerBlock}>
+                        <Link href="/faq" className={classes.footerLink}>
+                            FAQ
+                        </Link>
+
+                        <Link href="/write_to_use" className={classes.footerLink}>
+                            Write to use
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
