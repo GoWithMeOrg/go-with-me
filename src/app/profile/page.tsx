d5a5252f7e36b947cf3b728a3f539ef349ab99d6
banderlog2013 <banderlog2013@yandex.ru>
2024-07-12 16:39:22 +0300

    created page profile

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./page.module.css";
+
+export const Profile = () => {
+    return <div className={classes.container}></div>;
+};
+
+export default Profile;
