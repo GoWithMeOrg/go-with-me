3650714b939eade4e4cbd39a7fe101858f54c25b
AlexandrRazhev <alexandrrazh@gmail.com>
2024-07-29 17:23:25 +0300

    title component refactored

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,43 +1,17 @@
-import { FC, Fragment, HTMLProps, useMemo } from "react";
+import { createElement, FC, HTMLProps, useMemo } from "react";
 import styles from "./Title.module.css";
 
 interface TitleProps extends HTMLProps<HTMLHeadingElement> {
-    title: string;
-    tag?: "h1" | "h2" | "h3";
+    title?: string;
+    tag: "h1" | "h2" | "h3";
 }
 
 export const Title: FC<TitleProps> = ({ className, title, tag, ...rest }) => {
-    const hCssString: string = useMemo(() => {
-        let cssString = "";
-        if (tag === "h1") cssString += styles.h1;
-        if (tag === "h2") cssString += styles.h2;
-        if (tag === "h3") cssString += styles.h3;
-
+    const headingCssString: string = useMemo(() => {
+        let cssString = styles[tag];
         if (className) cssString += " " + className;
         return cssString;
     }, [className, tag]);
 
-    return (
-        <Fragment>
-            {tag === "h1" && (
-                <h1 className={hCssString} {...rest}>
-                    {title}
-                </h1>
-            )}
-
-            {tag === "h2" && (
-                <h2 className={hCssString} {...rest}>
-                    {title}
-                </h2>
-            )}
-
-            {tag === "h3" && (
-                <h3 className={hCssString} {...rest}>
-                    {title}
-                </h3>
-            )}
-        </Fragment>
-    );
+    return createElement(tag, { className: headingCssString, ...rest }, title);
 };
-
-export default Title;
