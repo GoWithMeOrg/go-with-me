9cf30d872cabfda9f0d794bd169673b7103d2dbe
banderlog2013 <banderlog2013@yandex.ru>
2024-07-30 21:22:41 +0300

    updated container

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,11 +1,22 @@
-import React from "react";
-import classes from "./Container.module.css";
+import React, { FC, useMemo } from "react";
+import styles from "./Container.module.css";
 
-type Props = {
-    children?: React.ReactNode;
-};
-export const Container = ({ children }: Props) => {
-    return <main className={classes.container}>{children}</main>;
+interface ContainerProps extends React.HTMLAttributes<HTMLDivElement> {
+    children: React.ReactNode;
+}
+export const Container: FC<ContainerProps> = ({ children, className, ...rest }) => {
+    const containerCssString: string = useMemo(() => {
+        let cssString = "";
+        cssString += styles.container;
+        if (className) cssString += " " + className;
+        return cssString;
+    }, [className]);
+
+    return (
+        <main className={containerCssString} {...{ ...rest }}>
+            <div className={styles.containerWidth}>{children}</div>
+        </main>
+    );
 };
 
 export default Container;
