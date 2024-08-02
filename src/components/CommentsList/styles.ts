7c8f124c1af116cc0bb8afd42f07f7fffd52357c
AlexandrRazhev <alexandrrazh@gmail.com>
2024-08-02 17:35:09 +0300

    context removed

---
+++ src/components/CommentsList/styles.ts
@@ -0,0 +1,13 @@
+import { IComment } from "@/database/models/Comment";
+import { IUser } from "@/database/models/User";
+import { Dispatch, SetStateAction } from "react";
+
+export interface ICommentProps extends Pick<IComment, "content" | "_id" | "createdAt" | "likes" | "replyToId"> {
+    author: Pick<IUser, "name">;
+    replies?: ICommentProps[];
+}
+
+export interface ICommentsListState {
+    replyIdState: string | null;
+    setReplyIdState: Dispatch<SetStateAction<string | null>>;
+}
