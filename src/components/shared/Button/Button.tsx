d869de6c12606e5c776c58862473e6ba17735ac3
zladen <zladen1983@gmail.com>
2024-08-25 14:58:28 +0300

    refactored button

--- src/components/shared/Button/Button.tsx
+++ src/components/shared/Button/Button.tsx
@@ -3,13 +3,12 @@ import { ButtonHTMLAttributes, FC, useMemo } from "react";
 import styles from "./Button.module.css";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-    text?: string;
-    icon?: React.ReactNode;
+    label?: string;
     resetDefaultStyles?: boolean;
     size?: "normal" | "big";
 }
 
-export const Button: FC<ButtonProps> = ({ text, icon, children, className, resetDefaultStyles, size, ...rest }) => {
+export const Button: FC<ButtonProps> = ({ label, children, className, resetDefaultStyles, size, ...rest }) => {
     const buttonCssString: string = useMemo(() => {
         let cssString = "";
         if (!resetDefaultStyles) cssString += styles.button;
@@ -21,10 +20,9 @@ export const Button: FC<ButtonProps> = ({ text, icon, children, className, reset
     }, [resetDefaultStyles, className, size]);
 
     return (
-        <button className={buttonCssString} {...{ ...rest }}>
-            {icon}
+        <button className={buttonCssString} {...rest}>
             {children}
-            {text}
+            {label}
         </button>
     );
 };
