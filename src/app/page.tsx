ba75d7ac7f5a33169800716b10b3050fddb3ca54
Pavel Andreev <zladen1983@gmail.com>
2024-11-16 10:46:13 +0300

    added component EventLisHome and styles for HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 "use client";
 
+import { EventListHome } from "@/components/shared/EvenListHome";
 import { Promo } from "@/components/shared/Promo";
+import { EventList } from "@/components/widgets/EventList";
 import { PopularEventList } from "@/components/widgets/PopularEventsList/PopularEventsList";
 
+import classes from "./page.module.css";
+
 export default function HomePage() {
-    return <Promo />;
+    return (
+        <div className={classes.homePage}>
+            <Promo />
+            <EventListHome />
+        </div>
+    );
     //return <PopularEventList />;
 }
---
+++ src/components/shared/EvenListHome/EventListHome.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+
+import { Title } from "../Title";
+import { CardEvent } from "@/components/widgets/CardEvent";
+import { IEvent } from "@/database/models/Event";
+import { Button } from "../Button";
+
+import ArrowCircle from "@/assets/icons/arrowCircle.svg";
+
+import { gql, useQuery } from "@apollo/client";
+import { Loader } from "../Loader";
+
+import classes from "./EventListHome.module.css";
+
+const GET_EVENTS = gql`
+    query GetEvents {
+        events {
+            _id
+            organizer {
+                _id
+                name
+                email
+                image
+            }
+
+            name
+            description
+            startDate
+            endDate
+            time
+            location {
+                type
+                coordinates
+                properties {
+                    address
+                }
+            }
+            image
+        }
+    }
+`;
+
+export const EventListHome = () => {
+    const { loading, error, data } = useQuery(GET_EVENTS);
+
+    if (loading) return <Loader />;
+    if (error) return <p>Error : {error.message}</p>;
+
+    return (
+        <section className={classes.cardsEvents}>
+            <Title title={"Popular Event List"} className={classes.cardsEventsTitle} tag={"h2"} />
+
+            <div className={classes.cardsEventsList}>
+                {data?.events.map(({ _id, description, name, startDate, location, time, image }: IEvent) => (
+                    <CardEvent
+                        key={_id}
+                        id={_id}
+                        name={name}
+                        description={description}
+                        coord={[location.coordinates[1], location.coordinates[0]]}
+                        startDate={startDate}
+                        time={time}
+                        image={image}
+                    />
+                ))}
+            </div>
+
+            <Button className={classes.eventsButton} resetDefaultStyles={true}>
+                More Events
+                <ArrowCircle style={{ marginRight: "1.25rem", marginLeft: "1rem" }} />
+            </Button>
+        </section>
+    );
+};
+
+export default EventListHome;
