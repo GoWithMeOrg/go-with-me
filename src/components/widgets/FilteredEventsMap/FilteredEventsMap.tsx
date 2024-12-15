a9867233080377d53a07aa530fe89d9ff986afda
Pavel Andreev <zladen1983@gmail.com>
2024-12-15 15:35:39 +0300

    added components to FilteredEventsMap

--- src/components/widgets/FilteredEventsMap/FilteredEventsMap.tsx
+++ src/components/widgets/FilteredEventsMap/FilteredEventsMap.tsx
@@ -2,18 +2,31 @@ import React, { useState } from "react";
 
 import { Title } from "@/components/shared/Title";
 
-import { GoogleMap } from "../GoogleMap";
+import { Autocomplete, GoogleMap } from "../GoogleMap";
 import NavbarEvents from "./NavbarEvents/NavbarEvents";
 import { FilterEvents } from "./FilterEvents";
 
 import classes from "./FilteredEventsMap.module.css";
+import { SelectCategory } from "../SelectCategory";
+import { eventCategory, eventTypes } from "@/components/shared/Dropdown/dropdownLists";
+import { CreateTag } from "@/components/widgets/CreateTag";
+import { Location } from "@/components/widgets/Location";
+import { Date } from "@/components/widgets/Date";
+import dayjs from "dayjs";
 export const FilteredEventsMap = () => {
     const [activeTab, setActiveTab] = useState("list");
+    const [selectedDate, setSelectedDate] = useState<string>("");
 
     const handleTabClick = (tab: string) => {
         setActiveTab(tab);
     };
 
+    const handleDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        //преобразуем дату обратно в iso
+        setSelectedDate(dayjs(e.target.value).toISOString());
+    };
+
+    console.log(selectedDate);
     return (
         <div className={classes.filteredEvents}>
             <div className={classes.header}>
@@ -25,11 +38,16 @@ export const FilteredEventsMap = () => {
             <div className={classes.line}></div>
 
             <div className={classes.filters}>
-                <div>Data</div>
-                <div>Locations</div>
-                <div>Categories</div>
-                <div>Subjects</div>
-                <div>Add tags</div>
+                <Date title={"Date"} onChange={handleDateChange} />
+                <Location onChange={() => {}} locationEvent={undefined} hideButtonMap={true} />
+                <SelectCategory categoryList={eventCategory} titleCategories={"Category"} onChange={() => {}} />
+                <SelectCategory categoryList={eventTypes} titleCategories={"Select category"} onChange={() => {}} />
+                <CreateTag
+                    eventTags={[]}
+                    onChange={function (e: string[]): void {
+                        throw new Error("Function not implemented.");
+                    }}
+                />
             </div>
 
             {activeTab === "list" && <FilterEvents />}
