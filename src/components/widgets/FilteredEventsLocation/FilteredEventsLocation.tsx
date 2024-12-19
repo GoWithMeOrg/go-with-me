d13245ac5ab16fa01e5737bbd5913b9244f99e56
Pavel Andreev <zladen1983@gmail.com>
2024-12-19 14:39:32 +0300

    added filteredLocation

---
+++ src/components/widgets/FilteredEventsLocation/FilteredEventsLocation.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import classes from "./FilteredEventsLocation.module.css";
+
+export const FilteredEventsLocation = () => {
+    return <div>FilteredEventsLocation</div>;
+};
+
+export default FilteredEventsLocation;
