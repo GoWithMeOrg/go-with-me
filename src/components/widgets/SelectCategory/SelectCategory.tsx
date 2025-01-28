4c1d51558ca2b8af31bc1b85fbed02573c57f7ba
Pavel Andreev <zladen1983@gmail.com>
2025-01-28 11:09:00 +0300

    deleted No Category from categoriesData

--- src/components/widgets/SelectCategory/SelectCategory.tsx
+++ src/components/widgets/SelectCategory/SelectCategory.tsx
@@ -2,10 +2,11 @@ import { forwardRef, useEffect, useRef, useState } from "react";
 
 import { Dropdown } from "@/components/shared/Dropdown";
 import { Label } from "@/components/shared/Label";
+import { Badges } from "@/components/shared/Badges";
+
+import Minus from "@/assets/icons/minus.svg";
 
 import classes from "./SelectCategory.module.css";
-import { Badges } from "@/components/shared/Badges";
-import Minus from "@/assets/icons/minus.svg";
 
 interface ISelectCategory {
     categoryList: string[];
@@ -30,13 +31,14 @@ export const SelectCategory = forwardRef(function SelectCategory(props: ISelectC
         setCategories(selectedCategories);
     };
 
+    console.log(categories);
     return (
         <div className={classes.selectedCategoriesLabel}>
             <Label className={classes.selectedCategoriesTitle} label={props.titleCategories} />
 
             <Dropdown
                 label={"No category"}
-                categoriesData={props.eventCategories || ["No category"]}
+                categoriesData={props.eventCategories || []}
                 onSelectedCategories={handleCategoryChange}
                 list={props.categoryList}
             />
