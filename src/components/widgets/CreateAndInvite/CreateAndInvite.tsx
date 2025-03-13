d0f488441a4e652050f27d72a9582dc77fa64590
Pavel Andreev <zladen1983@gmail.com>
2025-03-13 07:18:20 +0300

    styles popup

--- src/components/widgets/CreateAndInvite/CreateAndInvite.tsx
+++ src/components/widgets/CreateAndInvite/CreateAndInvite.tsx
@@ -63,7 +63,7 @@ export const CreateAndInvite: FC<CreateAndInviteProps> = ({ mode, status }) => {
                     style={{
                         backgroundColor: "#F7F7FA",
                         width: "30rem",
-                        height: "34.2rem",
+                        height: "24rem",
                         display: "flex",
                         justifyContent: "center",
                         alignItems: "center",
