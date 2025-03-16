6befb1369ab184aa0a032920cb2eb75659288188
Pavel Andreev <zladen1983@gmail.com>
2025-03-16 08:03:00 +0300

    added link from logo

--- src/components/widgets/Header/Header.tsx
+++ src/components/widgets/Header/Header.tsx
@@ -3,11 +3,15 @@ import SearchEvent from "@/components/widgets/SearchEvent/SearchEvent";
 import Logo from "@/assets/icons/logo.svg";
 
 import classes from "./Header.module.css";
+import Link from "next/link";
 
 export const Header = () => {
     return (
         <header className={classes.header}>
-            <Logo />
+            <Link href={"/events"}>
+                <Logo />
+            </Link>
+
             <div className={classes.headerAuth}>
                 <AuthPanel />
             </div>
