6c9d21f8b3f2e77bd082002a2486d6e7ed581979
Pavel Andreev <zladen1983@gmail.com>
2025-03-24 21:47:09 +0300

    added component ButtonNext

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -9,16 +9,39 @@ import { Mode } from "@/components/widgets/CreateAndInvite/CreateAndInvite";
 
 import { Title } from "@/components/shared/Title";
 import { Slider } from "@/components/shared/Slider";
+import { Carousel } from "@/components/shared/Carousel";
 
 import classes from "./page.module.css";
+import { useSlider } from "@/components/shared/Slider/hook/useSlider";
+import { IEvent } from "@/database/models/Event";
+import { Slide } from "@/components/shared/Slider/Slide";
 
 const EventListPage: NextPage = () => {
     const { status } = useSession();
+    const { hideSliderHandler, hideSlider, currentIndex, filterEventsImage, slideWidth, nextSlide } = useSlider();
+
+    const arr = filterEventsImage.length;
 
     return (
         <div className={classes.eventListPage}>
             <Title tag={"h1"} title="Присоединяйтесь к приключениям" className={classes.title} />
-            <Slider />
+            {/* <Slider /> */}
+            {/* <div style={{ width: "100%", marginLeft: "auto", marginRight: "auto", marginTop: 64 }}> */}
+            <Carousel show={3} arr={arr}>
+                {filterEventsImage.map((slide: IEvent) => (
+                    <Slide
+                        key={slide._id}
+                        id={slide._id}
+                        name={slide.name}
+                        image={slide.image as string}
+                        startDate={slide.startDate as Date}
+                        time={slide.time as string}
+                        coord={[slide.location.coordinates[0], slide.location.coordinates[1]]}
+                    />
+                ))}
+            </Carousel>
+            {/* </div> */}
+
             <EventFilters />
             <CreateAndInvite mode={Mode.EVENT} status={status} />
         </div>
--- src/components/shared/Carousel/Carousel.tsx
+++ src/components/shared/Carousel/Carousel.tsx
@@ -1,14 +1,71 @@
+import { FC, useEffect, useState } from "react";
+
+import { Title } from "@/components/shared/Title";
+import { Button } from "@/components/shared/Button";
+import { ButtonNext } from "@/components/shared/ButtonNext";
+
 import classes from "./Carousel.module.css";
 
-export const Carousel = (props: any) => {
-    const { children } = props;
+interface ICarousel {
+    children: React.ReactNode[];
+}
+
+export const Carousel: FC<ICarousel> = ({ children }) => {
+    const [width, setWidth] = useState<number | null>(null);
+    const [hideSlider, setHideSlider] = useState(false);
+    const [currentIndex, setCurrentIndex] = useState<number>(0);
+    const [length, setLength] = useState<number>(children.length);
+
+    const hideSliderHandler = () => {
+        setHideSlider(true);
+    };
+
+    const next = () => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % length);
+    };
+
+    useEffect(() => {
+        setLength(children.length);
+    }, [children]);
+
+    useEffect(() => {
+        const updateWidth = () => {
+            const windowInnerWidth = window.innerWidth;
+            const containerGrid = 1180;
+            setWidth((windowInnerWidth - containerGrid) / 2 + containerGrid);
+        };
+
+        updateWidth();
+        window.addEventListener("resize", updateWidth);
+
+        return () => window.removeEventListener("resize", updateWidth);
+    }, []);
+
     return (
-        <div className={classes.container}>
-            <div className={classes.wrapper}>
-                <div className={classes.contetnWrapper}>
-                    <div className={classes.content}>{children}</div>
+        <>
+            {!hideSlider && (
+                <div className={classes.container}>
+                    <div className={classes.header}>
+                        <Title tag={"h3"} title="Рекомендуемые события" />
+                        <Button resetDefaultStyles={true} className={classes.hide} onClick={hideSliderHandler}>
+                            СКРЫТЬ
+                        </Button>
+                    </div>
+
+                    <div className={classes.wrapper} style={{ maxWidth: `${width}px` }}>
+                        <div className={classes.contentWrapper}>
+                            <div
+                                className={classes.content}
+                                style={{ transform: `translateX(-${currentIndex * (100 / length)}%)` }}
+                            >
+                                {children}
+                            </div>
+                        </div>
+                    </div>
+
+                    <ButtonNext onNext={next} />
                 </div>
-            </div>
-        </div>
+            )}
+        </>
     );
 };
