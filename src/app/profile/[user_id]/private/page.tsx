918d59178915df9aa54ca1f1e91e824fb936c37e
Pavel Andreev <zladen1983@gmail.com>
2025-04-17 09:39:38 +0300

    new routes have been added

---
+++ src/app/profile/[user_id]/private/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import React from "react";
+import { NextPage } from "next";
+
+import { ProfileTabs } from "@/components/widgets/ProfileTabs";
+//import { Avatar } from "@/components/widgets/";
+
+import { Button } from "@/components/shared/Button";
+import { Title } from "@/components/shared/Title";
+
+import Arrow from "@/assets/icons/arrow.svg";
+
+import classes from "./page.module.css";
+
+const Profile: NextPage = () => {
+    return (
+        <div className={classes.profilePage}>
+            <div className={classes.formWrapper}>
+                <Button className={classes.arrowButton} resetDefaultStyles={true}>
+                    <Arrow />
+                </Button>
+                <div className={classes.avatarTitle}>
+                    {/* <Avatar name={""} /> */}
+                    <Title className={classes.formTitle} title="MY ACCOUNT" tag={"h2"} />
+                </div>
+                <ProfileTabs />
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
