e7ea9133abbdfa4c8084d5a71ab0f4b4b56d5892
Max Makarov <makarov9502@gmail.com>
2025-07-01 23:31:58 +0300

    add button back

--- src/app/profile/[user_id]/private/page.tsx
+++ src/app/profile/[user_id]/private/page.tsx
@@ -6,10 +6,8 @@ import { NextPage } from "next";
 import { ProfileTabs } from "@/components/widgets/ProfileTabs";
 //import { Avatar } from "@/components/widgets/";
 
-import { Button } from "@/components/shared/Button";
+import { ButtonBack } from "@/components/shared/ButtonBack";
 import { Title } from "@/components/shared/Title";
-
-import Arrow from "@/assets/icons/arrow.svg";
 
 import classes from "./page.module.css";
 
@@ -17,9 +15,7 @@ const Profile: NextPage = () => {
     return (
         <div className={classes.profilePage}>
             <div className={classes.formWrapper}>
-                <Button className={classes.arrowButton} resetDefaultStyles={true}>
-                    <Arrow />
-                </Button>
+                <ButtonBack />
                 <div className={classes.avatarTitle}>
                     <Title className={classes.formTitle} title="MY ACCOUNT" tag={"h2"} />
                 </div>
