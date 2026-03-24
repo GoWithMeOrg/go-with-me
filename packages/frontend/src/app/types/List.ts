de3570308d7c6cc8a617a5e922f30e568a3d7293
Pavel <zladen1983@gmail.com>
2026-03-24 15:59:58 +0300

    partially deleted old types

--- packages/frontend/src/app/types/List.ts
+++ packages/frontend/src/app/types/List.ts
@@ -1,16 +1,16 @@
-import { IUser } from './User';
+import { User } from '@/app/graphql/types';
 
 export interface IList {
-  _id?: string;
-  author_id: string;
-  name: string;
-  description: string;
-  users_id: string[];
+    _id?: string;
+    author_id: string;
+    name: string;
+    description: string;
+    users_id: string[];
 }
 
 export interface IListInput {
-  name: string;
-  description: string;
-  users_id: string[];
-  users: IUser[];
+    name: string;
+    description: string;
+    users_id: string[];
+    users: User[];
 }
